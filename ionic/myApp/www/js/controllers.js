813a3c2ace8b94167e6ffc7d32cceb24a2c37dcd
unknown <srawal@dlugsin2402060.sapient.com>
2015-02-28 10:17:11 +0530

    search changes

--- ionic/myApp/www/js/controllers.js
+++ ionic/myApp/www/js/controllers.js
@@ -20,6 +20,12 @@ angular.module('starter.controllers', [])
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
+.controller('MyController', ['$scope', '$http', function($scope, $http) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data;
+    $scope.artistOrder = 'name';
+  });
+}])
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
@@ -33,71 +39,7 @@ angular.module('starter.controllers', [])
   };
 });*/
 
-factory('Flickr', function($resource, $q) {
-  var photosPublic = $resource('http://api.flickr.com/services/feeds/photos_public.gne', 
-      { format: 'json', jsoncallback: 'JSON_CALLBACK' }, 
-      { 'load': { 'method': 'JSONP' } });
-      
-  return {
-    search: function(query) {
-      var q = $q.defer();
-      photosPublic.load({
-        tags: query
-      }, function(resp) {
-        q.resolve(resp);
-      }, function(err) {
-        q.reject(err);
-      })
-      
-      return q.promise;
-    }
-  }
-})
 
-.controller('FlickrCtrl', function($scope, Flickr) {
-
-  var doSearch = ionic.debounce(function(query) {
-    Flickr.search(query).then(function(resp) {
-      $scope.photos = resp;
-    });
-  }, 500);
-  
-  $scope.search = function() {
-    doSearch($scope.query);
-  }
-
-})
-
-.directive('pushSearch', function() {
-  return {
-    restrict: 'A',
-    link: function($scope, $element, $attr) {
-      var amt, st, header;
-
-      $element.bind('scroll', function(e) {
-        if(!header) {
-          header = document.getElementById('search-bar');
-        }
-        st = e.detail.scrollTop;
-        if(st < 0) {
-          header.style.webkitTransform = 'translate3d(0, 0px, 0)';
-        } else {
-          header.style.webkitTransform = 'translate3d(0, ' + -st + 'px, 0)';
-        }
-      });
-    }
-  }
-})
-
-.directive('photo', function($window) {
-  return {
-    restrict: 'C',
-    link: function($scope, $element, $attr) {
-      var size = ($window.outerWidth / 3) - 2;
-      $element.css('width', size + 'px');
-    }
-  }
-});
 
 
 
